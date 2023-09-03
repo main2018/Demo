eff60868a3ae2b5e44ae2db2997bafc852d0abac
main <375277023@qq.com>
2023-09-03 22:48:13 +0800

    add vite test

---
+++ Vite/vite-mpa-test/src/pages/test1/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    test1
+    <img src="/vite.svg" class="logo" alt="Vite logo" />
+    <router-link to="/detail">Detail</router-link>
+    <router-view></router-view>
+  </div>    
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
