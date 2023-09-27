17ae6b427e6f445cda2d13c2624664f91b7d616a
wangpan <pan.wang@ushow.media>
2023-09-27 15:21:07 +0800

    增加webpack热更新/多页应用

---
+++ Webpack/vue-multi-pages/src/page1/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="my_app" class="wrapper">
+		<div class="zhubo-main">
+			<div class="zhubo-tab">
+				<ul class="tab-nav">
+					Hello Page1
+				</ul>
+			</div>
+			<div class="zhubo-tab-cont">
+			</div>
+		</div>
+		input: <input type="text" placeholder="输入看页面修改会不会消失">
+	</div>
+</template>  
+
+<script>
+export default {
+	name: 'App',
+	components: {},
+}
+</script>
+<style>
+</style>
---
+++ Webpack/vue-multi-pages/src/page1/main.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: wangpan pan.wang@ushow.media
+ * @Date: 2018-04-08 19:23:08
+ * @LastEditors: wangpan pan.wang@ushow.media
+ * @LastEditTime: 2023-09-27 15:19:55
+ * @FilePath: /code-demo/Webpack/vue-multi-pages/src/page1/main.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import Vue from 'vue'
+import App from './App'
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
